64ca1c3a910811631a3b6c12e4c5be4540dd8931
dmitriyn <d.nikitin.rf@gmail.com>
2025-03-27 12:19:05 +0500

    upd project

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,44 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+//import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.text_home), withText("This is home fragment"),
+                        withParent(withParent(withId(R.id.nav_host_fragment_content_main))),
+                        isDisplayed()));
+        textView.check(matches(withText("This is home fragment")));
+    }
+
+    @Test
+    public void mainHomeTest() {
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(withText("This is home fragment")));
+    }
+}
